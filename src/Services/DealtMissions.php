29cff7c7155c2e3cf57b07edf2e9075b46323f5f
Edvin CANDON <edvincandon@gmail.com>
2022-03-21 12:26:55 +0100

    chore: improve phpdoc in services

--- src/Services/DealtMissions.php
+++ src/Services/DealtMissions.php
@@ -10,16 +10,36 @@ use Dealt\DealtSDK\GraphQL\Types\Input\SubmitMissionMutationCustomer;
 use Dealt\DealtSDK\GraphQL\Types\Object\SubmitMissionMutationFailure;
 use Dealt\DealtSDK\GraphQL\Types\Object\SubmitMissionMutationSuccess;
 
+/**
+ * DealtMissions Service
+ * - submit mission
+ * - cancel mission
+ * - missions
+ * - mission.
+ */
 class DealtMissions extends AbstractDealtService
 {
     /**
      * Posts a mission to the Dealt API.
      *
-     * @param array<string, mixed> $params SubmitMissionMutation parameters
-     *                                     - offer_id(string) : the uuid of the dealt offer
-     *                                     - address(array<string, mixed>) : customer address
-     *                                     - customer(array<string, mixed>) : customer description
-     *                                     - webhook(?string) : optional webhook url
+     * @param array{
+     *      offer_id: string,
+     *      address: array{
+     *          country: string,
+     *          zip_code: string,
+     *          city?: string,
+     *          street1?: string,
+     *          street2?: string,
+     *          company?: string
+     *      },
+     *      customer: array{
+     *          first_name: string,
+     *          last_name: string,
+     *          email_address: string,
+     *          phone_number: string
+     *      },
+     *      webhook?: string
+     * } $params SubmitMissionMutation parameters
      *
      * @throws GraphQLFailureException
      *
@@ -28,18 +48,13 @@ class DealtMissions extends AbstractDealtService
     public function submit(array $params): GraphQLObjectInterface
     {
         $mutation = new SubmitMissionMutation();
-        if (isset($params['offer_id'])) {
-            $mutation->setQueryVar('offerId', $params['offer_id']);
-        }
-        if (isset($params['address'])) {
-            $mutation->setQueryVar('address', SubmitMissionMutationAddress::fromArray($params['address']));
-        }
-        if (isset($params['customer'])) {
-            $mutation->setQueryVar('customer', SubmitMissionMutationCustomer::fromArray($params['customer']));
-        }
-        if (isset($params['webhook'])) {
-            $mutation->setQueryVar('webhook', $params['webhook']);
-        }
+        self::validateParameters($params, $mutation);
+
+        $mutation->setQueryVar('offerId', $params['offer_id']);
+        $mutation->setQueryVar('address', SubmitMissionMutationAddress::fromArray($params['address']));
+        $mutation->setQueryVar('customer', SubmitMissionMutationCustomer::fromArray($params['customer']));
+
+        if (isset($params['webhook'])) $mutation->setQueryVar('webhook', $params['webhook']);
 
         /** @var SubmitMissionMutationSuccess|SubmitMissionMutationFailure */
         $result = $this->getGQLClient()->exec($mutation);
--- src/Services/DealtOffers.php
+++ src/Services/DealtOffers.php
@@ -8,7 +8,6 @@ use Dealt\DealtSDK\GraphQL\Queries\OfferAvailabilityQuery;
 use Dealt\DealtSDK\GraphQL\Types\Input\OfferAvailabilityQueryAddress;
 use Dealt\DealtSDK\GraphQL\Types\Object\OfferAvailabilityQueryFailure;
 use Dealt\DealtSDK\GraphQL\Types\Object\OfferAvailabilityQuerySuccess;
-use Exception;
 
 class DealtOffers extends AbstractDealtService
 {
@@ -16,29 +15,36 @@ class DealtOffers extends AbstractDealtService
      * Resolve the availability for a given offer id and
      * a customer address (at least a country and a zipCode).
      *
-     * @param array<string, mixed> $params OfferAvailabilityQuery parameters
-     *                                     - offer_id(string) : the uuid of the dealt offer
-     *                                     - address(array<string, mixed>) : customer address
+     * @param array{
+     *      offer_id: string,
+     *      address: array{
+     *          country: string,
+     *          zip_code: string,
+     *          city?: string,
+     *          street1?: string,
+     *          street2?: string,
+     *          company?: string
+     *      }
+     * } $params OfferAvailabilityQuery parameters
      *
      * @throws GraphQLFailureException|GraphQLException
      */
     public function availability(array $params): OfferAvailabilityQuerySuccess
     {
-        try {
-            $query = new OfferAvailabilityQuery();
-            $query->setQueryVar('offerId', $params['offer_id']);
-            $query->setQueryVar('address', OfferAvailabilityQueryAddress::fromArray($params['address']));
+        $query = new OfferAvailabilityQuery();
+        self::validateParameters($params, $query);
 
-            /** @var OfferAvailabilityQuerySuccess|OfferAvailabilityQueryFailure */
-            $result = $this->getGQLClient()->exec($query);
+        $query = new OfferAvailabilityQuery();
+        $query->setQueryVar('offerId', $params['offer_id']);
+        $query->setQueryVar('address', OfferAvailabilityQueryAddress::fromArray($params['address']));
 
-            if ($result instanceof OfferAvailabilityQueryFailure) {
-                throw new GraphQLFailureException($result->reason);
-            }
+        /** @var OfferAvailabilityQuerySuccess|OfferAvailabilityQueryFailure */
+        $result = $this->getGQLClient()->exec($query);
 
-            return $result;
-        } catch (Exception $e) {
-            throw new GraphQLException('something went wrong');
+        if ($result instanceof OfferAvailabilityQueryFailure) {
+            throw new GraphQLFailureException($result->reason);
         }
+
+        return $result;
     }
 }
